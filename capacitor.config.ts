03b68fcd67ab05c847e8daa63aef5610067afd5b
Sebastián Rodrigo Nicolás Kravetz Lagneto <skravetz@internacional.cl>
2023-08-29 20:48:56 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'carsharing_formab',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
