c9aed70e33b8fe81d3bc3f3bf8fe03ee018f6997
Sebastián Kravetz <skravetz@internacional.cl>
2023-10-04 08:51:52 -0300

    [@wwiiddeeweb][feature/repositories] implementing repository pattern for db provider .

--- src/app/providers/db-api/api.service.ts
+++ src/app/providers/db-api/api.service.ts
@@ -1,51 +1,6 @@
-import { HttpClient, HttpResponseBase } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { UserModel } from '../../routes/login/model/user.model';
-import { supabaseHeaders } from './api.config';
-import { ApiConstants } from './api.constants';
-import { AddUserRequest, EditUserRequest } from './model/api.model';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ApiService {
-  constructor(private httpClient: HttpClient) {}
-
-  getUsers(): Observable<UserModel[]> {
-    return this.httpClient.get<UserModel[]>(ApiConstants.PATH_USERS, {
-      headers: supabaseHeaders,
-      responseType: 'json',
-    });
-  }
-
-  getUserByEmail(email: string): Observable<UserModel[]> {
-    return this.httpClient.get<UserModel[]>(
-      `${ApiConstants.PATH_USERS}?${ApiConstants.USERBYEMAIL_QUERY_PARAM}=eq.${email}`,
-      {
-        headers: supabaseHeaders,
-        responseType: 'json',
-      }
-    );
-  }
-
-  addUser(payload: AddUserRequest): Observable<Object> {
-    return this.httpClient.post(ApiConstants.PATH_USERS, payload, {
-      headers: supabaseHeaders,
-      responseType: 'json',
-    });
-  }
-
-  editUser(payload: EditUserRequest): Observable<HttpResponseBase> {
-    const userID = payload.id;
-    return this.httpClient.put(
-      `${ApiConstants.PATH_USERS}?id=eq.${userID}`,
-      payload,
-      {
-        headers: supabaseHeaders,
-        responseType: 'json',
-        observe: 'response',
-      }
-    );
-  }
-}
+export class ApiService {}
