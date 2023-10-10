26f632e51e773b4bb322ca4aa1601afa858aaa40
ponytakxn <ju.valdesv@duocuc.cl>
2023-10-09 23:30:00 -0300

    @ponytakxn feat updating style vars

--- tailwind/plugins/input.js
+++ tailwind/plugins/input.js
@@ -3,21 +3,9 @@ const plugin = require('tailwindcss/plugin')
 module.exports = plugin(function ({ addUtilities }) {
   const inputClasses = {
     '.input-primary': {
-        padding: '0 0.5rem !important',
-        fontWidth: 'bold',
-        '&:focus': {
-          background: '#a08dfb',
-          color: '#141520'
-        },
-        '&:hover': {
-          background: '#a08dfb',
-          color: '#141520'
-        }
-    },
-    '.input-secondary': {
       padding: '0 0.5rem !important',
-      fontWidth: 'bold',
-  },
+      fontWeight: '600 !important',
+    }
   }
   addUtilities(inputClasses)
 })
