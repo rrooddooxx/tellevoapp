f83683df0df4129c38a338a36fd407c9ac49fc93
Sebastián Kravetz <sebastiankravetz@icloud.com>
2023-10-21 13:51:45 -0300

    .

--- src/app/components/trip-card/trip-card.component.ts
+++ src/app/components/trip-card/trip-card.component.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { ITripCardType } from '../../shared/enums/trip-card.enum';
 
 @Component({
+  standalone: true,
   selector: 'app-trip-card',
   templateUrl: './trip-card.component.html',
   styleUrls: ['./trip-card.component.scss'],
+  imports: [CommonModule, IonicModule],
 })
 export class TripCardComponent implements OnInit {
   @Input() type: ITripCardType;
-  TripCardType: ITripCardType;
+  tripCardType = ITripCardType;
   constructor() {}
 
   ngOnInit() {}
