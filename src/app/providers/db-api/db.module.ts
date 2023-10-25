6efc95f347aaae8259734b9c4bbed781c5903b6f
Sebastián Kravetz <skravetz@internacional.cl>
2023-10-25 00:28:23 -0300

    .

--- src/app/providers/db-api/db.module.ts
+++ src/app/providers/db-api/db.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TripsRepositoryConfig } from './config/trips.config';
 import { CareerRepository } from './repositories/careers.repository';
 import { TripsRepository } from './repositories/trips.repository';
 import { UsersRepository } from './repositories/users.repository';
@@ -7,6 +8,11 @@ import { UsersRepository } from './repositories/users.repository';
 @NgModule({
   declarations: [],
   imports: [CommonModule],
-  providers: [UsersRepository, TripsRepository, CareerRepository],
+  providers: [
+    UsersRepository,
+    TripsRepository,
+    CareerRepository,
+    TripsRepositoryConfig,
+  ],
 })
 export class DbModule {}
