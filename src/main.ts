7bddc32d265c961fd9a402c95708d295761c007e
Sebastián Kravetz <sebastiankravetz@icloud.com>
2023-10-28 05:08:39 -0300

    .

--- src/main.ts
+++ src/main.ts
@@ -3,29 +3,18 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
-import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
 import { environment } from './environments/environment';
-import { hmrBootstrap } from './hmr';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const bootstrap = bootstrapApplication(AppComponent, {
+bootstrapApplication(AppComponent, {
   providers: [
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     importProvidersFrom(IonicModule.forRoot({})),
     provideRouter(routes),
   ],
 });
-
-if (environment.hmr) {
-  if (module['hot']) {
-    hmrBootstrap(module, bootstrap);
-  } else {
-    console.error('HMR is not enabled for webpack-dev-server!');
-    console.log('Are you using the --hmr flag for ng serve?');
-  }
-} else {
-}
