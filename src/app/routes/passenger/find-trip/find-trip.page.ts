050e777333de6ca241dfaf837581f1e50eecd776
Pony <juan.valdes@applydigital.com>
2023-10-29 14:38:02 -0300

    reset main

--- src/app/routes/passenger/find-trip/find-trip.page.ts
+++ src/app/routes/passenger/find-trip/find-trip.page.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { TripCardComponent } from '../../../components/trip-card/trip-card.component';
-import { ITripCardState, ITripStatus } from '../../../components/trip-card/trip-card.interfaces';
+import { ITripCardState } from '../../../components/trip-card/trip-card.interfaces';
 import { TripListItemComponent } from '../../../components/trip-list-item/trip-list-item.component';
 import { DbModule } from '../../../providers/db-api/db.module';
 import { TripMappers } from '../../../providers/db-api/mappers/trips.mappers';
@@ -32,7 +32,7 @@ export class FindTripPage implements OnInit {
   constructor(
     private readonly tripsRepository: TripsRepository,
     private readonly mapper: TripMappers
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.getActiveTrips();
