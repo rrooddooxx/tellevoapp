a19321cf7b40d03427030479c1da823cf3366c75
Sebastián Kravetz <sebastiankravetz@icloud.com>
2023-10-29 14:28:33 -0300

    .

--- scripts/clearcommit.sh
+++ scripts/clearcommit.sh
@@ -4,19 +4,22 @@ if ! git rev-parse --is-inside-work-tree > /dev/null 2>&1; then
   exit 1
 fi
 
-file1="/src/environments/environment.ts"
-file2="/src/environments/environment.prod.ts"
+file1="./src/environments/environment.ts"
+file2="./src/environments/environment.prod.ts"
+
+echo "Checking file: $(realpath $file1)"
+echo "Checking file: $(realpath $file2)"
 
 if [ -f "$file1" ]; then
   echo "DEV Environment file exists. Deleting it before commit..."
-  git rm "$file1"
+  rm "$file1"
   echo "File deleted."
 else
   echo "File do not exist. Aborting."
 fi
 if [ -f "$file1" ]; then
   echo "PROD Environment file exists. Deleting it before commit..."
-  git rm "$file2"
+  rm "$file2"
   echo "File deleted."
 else
   echo "File do not exist. Aborting."
