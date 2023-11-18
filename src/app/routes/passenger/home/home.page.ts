ca02ad28bae82514d0c8bb5b2843c773798e5084
Sebastián Kravetz <70353160+wwiiddeeweb@users.noreply.github.com>
2023-11-18 10:22:13 -0300

    . (#36)

--- src/app/routes/passenger/home/home.page.ts
+++ src/app/routes/passenger/home/home.page.ts
@@ -1,7 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import {
+  CUSTOM_ELEMENTS_SCHEMA,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { GoogleMap, Polyline } from '@capacitor/google-maps';
 import { IonicModule, NavController } from '@ionic/angular';
 import { TabnavComponent } from '../../../components/tabnav/tabnav.component';
 import { IPassengerState } from '../../../stores/passenger/passenger.interfaces';
@@ -14,8 +21,12 @@ import { UserModel } from '../../login/model/user.model';
   styleUrls: ['./home.page.scss'],
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule, TabnavComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class HomePage implements OnInit {
+  @ViewChild('map')
+  mapRef: ElementRef<HTMLElement>;
+  newMap: GoogleMap;
   public userInfo: UserModel = {} as UserModel;
   public currentState: IPassengerState;
 
@@ -25,8 +36,51 @@ export class HomePage implements OnInit {
     private readonly navCtrl: NavController
   ) {}
 
-  ngOnInit() {
+  async createMap() {
+    this.newMap = await GoogleMap.create({
+      id: 'my-cool-map',
+      element: this.mapRef.nativeElement,
+      apiKey: 'AIzaSyBXCBehWv7oDlFgnwwtN_AoQhM8u-QJ1RM',
+      config: {
+        center: {
+          lat: -33.033672,
+          lng: -71.533049,
+        },
+        zoom: 18,
+      },
+    });
+    this.newMap.addMarker({
+      coordinate: {
+        lat: -33.033921,
+        lng: -71.532663,
+      },
+    });
+    this.newMap.addMarker({
+      coordinate: {
+        lat: -33.033333,
+        lng: -71.531663,
+      },
+    });
+    const lines: Polyline[] = [
+      {
+        path: [
+          { lat: -33.033921, lng: -71.532663 },
+          {
+            lat: -33.033333,
+            lng: -71.531663,
+          },
+        ],
+      },
+    ];
+    this.newMap.addPolylines(lines);
+  }
+
+  async ngOnInit() {
     this.currentState = this.passengerStore.getState();
+  }
+
+  async ngAfterViewInit() {
+    this.createMap();
   }
 
   goToAvailableTrips() {
